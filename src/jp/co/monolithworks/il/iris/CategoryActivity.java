69116283254a7abaa165d185e93b6bb6c8154a70
Yuusuke Sakuma <yuusuke_s@monolithworks.co.jp>
2012-09-19 14:26:39 +0900

    add intent

--- src/jp/co/monolithworks/il/iris/CategoryActivity.java
+++ src/jp/co/monolithworks/il/iris/CategoryActivity.java
@@ -6,7 +6,7 @@ import android.view.Menu;
 import android.widget.GridView;
 import android.view.View.OnClickListener;
 import android.widget.BaseAdapter;
-import android.content.Context;
+import android.content.*;
 import android.view.ViewGroup;
 import android.view.View;
 import android.widget.ImageView;
@@ -26,7 +26,10 @@ public class CategoryActivity extends Activity {
 
         gridview.setOnItemClickListener(new OnItemClickListener() {
             public void onItemClick(AdapterView parent, View v, int position, long id) {
-                Toast.makeText(CategoryActivity.this, "" + position, Toast.LENGTH_SHORT).show();
+                //Toast.makeText(CategoryActivity.this, "" + position, Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent();
+                setResult(RESULT_OK,intent);
+                finish();
             }
         });
     }
@@ -70,19 +73,5 @@ public class CategoryActivity extends Activity {
             imageView.setImageResource(android.R.drawable.sym_def_app_icon);
             return imageView;
         }
-
-        //private Integer[] mThumbIds = {
-        //  R.drawable.sample_2, R.drawable.sample_3,
-        //  R.drawable.sample_4, R.drawable.sample_5,
-        //  R.drawable.sample_6, R.drawable.sample_7,
-        //  R.drawable.sample_0, R.drawable.sample_1,
-        //  R.drawable.sample_2, R.drawable.sample_3,
-        //  R.drawable.sample_4, R.drawable.sample_5,
-        //  R.drawable.sample_6, R.drawable.sample_7,
-        //  R.drawable.sample_0, R.drawable.sample_1,
-        //  R.drawable.sample_2, R.drawable.sample_3,
-        //  R.drawable.sample_4, R.drawable.sample_5,
-        //  R.drawable.sample_6, R.drawable.sample_7
-        //};
     }
 }
